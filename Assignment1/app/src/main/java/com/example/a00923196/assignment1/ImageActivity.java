7031c3c17282f67afc50402e4ff8c0e421fcea42
Niko Lauron <A00923196@ad.bcit.ca>
2017-05-25 20:17:14 -0700

    Assignment 1

--- Assignment1/app/src/main/java/com/example/a00923196/assignment1/ImageActivity.java
+++ Assignment1/app/src/main/java/com/example/a00923196/assignment1/ImageActivity.java
@@ -3,8 +3,8 @@ package com.example.a00923196.assignment1;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 public class ImageActivity extends AppCompatActivity {
