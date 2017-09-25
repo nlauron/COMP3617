388952715591729732028864d4320a86eca7e8eb
Niko <nlauron@telus.net>
2017-09-24 19:52:39 -0700

    Unity

---
+++ Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public float force = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().AddForce(Vector3.right * force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
