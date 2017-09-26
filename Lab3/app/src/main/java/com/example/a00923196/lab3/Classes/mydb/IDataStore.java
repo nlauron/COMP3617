1bf8a8f267f5392c66da8896e148a53da17f446d
nlauron <nlauron@telus.net>
2017-09-26 15:07:41 -0700

    lab work

---
+++ Lab3/app/src/main/java/com/example/a00923196/lab3/Classes/mydb/IDataStore.java
@@ -0,0 +1,10 @@
+package com.example.a00923196.lab3.Classes.mydb;
+
+/**
+ * Created by A00923196 on 2017-09-19.
+ */
+
+public interface IDataStore {
+    void saveState(String state);
+    String getState();
+}
---
+++ Lab3/app/src/main/java/com/example/a00923196/lab3/MyApplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a00923196.lab3.MyApplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.a00923196.lab3.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
