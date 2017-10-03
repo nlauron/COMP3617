689fbd5db095b0a83ea87825f6b0dfc62319f43f
Niko <nlauron@telus.net>
2017-10-03 00:12:59 -0700

    android

---
+++ PhotoApp/app/src/main/java/com/example/nikolauron/photoapp/Cell.java
@@ -0,0 +1,27 @@
+package com.example.nikolauron.photoapp;
+
+/**
+ * Created by Niko Lauron on 10/2/2017.
+ */
+
+public class Cell {
+
+    private String title;
+    private Integer img;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getImg() {
+        return img;
+    }
+
+    public void setImg(Integer img) {
+        this.img = img;
+    }
+}
---
+++ PhotoApp/app/src/main/java/com/example/nikolauron/photoapp/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.nikolauron.photoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String image_titles[] = {
+            "Img1",
+            "Img2",
+            "Img3",
+            "Img4",
+            "Img5",
+            "Img6",
+            "Img7",
+            "Img8"
+    };
+
+    private final Integer image_ids[] = {
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3,
+            R.drawable.image4,
+            R.drawable.image5,
+            R.drawable.image6,
+            R.drawable.image7,
+            R.drawable.image8
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.gallery);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<Cell> cells = prepareData();
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), cells);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private ArrayList<Cell> prepareData() {
+        ArrayList<Cell> theimage = new ArrayList<>();
+        for (int i = 0; i < image_titles.length; i++) {
+            Cell cell = new Cell();
+            cell.setTitle(image_titles[i]);
+            cell.setImg(image_ids[i]);
+            theimage.add(cell);
+        }
+        return theimage;
+    }
+}
